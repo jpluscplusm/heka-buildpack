fde52c08de846783a7dca327a36e91104ff38ec5
Jonathan Matthews <contact@jpluscplusm.com>
2016-01-13 11:19:35 +0000

    Add envvar controls of basic config options

--- bin/run.sh
+++ bin/run.sh
@@ -1,8 +1,14 @@
-set -x
+#!/bin/bash
+[[ ${SHELL_TRACE} ]] && set -x
+set -euo pipefail
+
 export APP_ROOT=$HOME
 : ${HEKA_SHARE_DIR:=${APP_ROOT}/heka/share/heka}
 : ${HEKA_BASE_DIR:=/tmp}
+: ${HEKA_MAX_PROCS:=2}
+
 export HEKA_SHARE_DIR
 export HEKA_BASE_DIR
+export HEKA_MAX_PROCS
 
 exec ${APP_ROOT}/heka/bin/hekad -config="${APP_ROOT}/heka/conf/"
